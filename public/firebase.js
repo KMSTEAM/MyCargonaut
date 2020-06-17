ca2bd37f8198a8067a5eb964124e0867733b168f
Jonas Kuche <jonas.kuche@gmail.com>
2020-06-17 10:35:17 +0200

    Add firebase integration

---
+++ public/firebase.js
@@ -0,0 +1,160 @@
+/**
+ * A class Handling the Firebase integration for Cargonaut
+ */
+class FirebaseIntegration {
+
+  /**
+   * Creates a new Firebase Integration Object
+   * @param projectID {string}
+   * @param apiKey {string}
+   * @param sendID {string}
+   * @param appID {string}
+   * @param measurementID {string}
+   */
+  constructor(projectID, apiKey, sendID, appID, measurementID) {
+    this.firebaseConfig = {
+      apiKey: apiKey,
+      authDomain: `${projectID}.firebaseapp.com`,
+      databaseURL: `https://${projectID}.firebaseio.com`,
+      projectId: projectID,
+      storageBucket: `${projectID}.appspot.com`,
+      messagingSenderId: sendID,
+      appId: appID,
+      measurementId: measurementID,
+    };
+  }
+
+  init() {
+    return firebase.initializeApp(this.firebaseConfig);
+  }
+
+  /**
+   * Registers a Cargonaut user in firebase
+   * @param email {string}
+   * @param password {string}
+   * @param username {string}
+   * @param birthDate {Date}
+   * @param profilePicture {File}
+   * @returns {Promise<void>}
+   */
+  registerUser(email, password, username, birthDate, profilePicture) {
+    let _user;
+    return firebase.auth().createUserWithEmailAndPassword(email, password).then(({user}) => {
+      _user = user;
+      const storageRef = firebase.storage().ref(user.uid + '/profilePicture/' + profilePicture.name);
+      return storageRef.put(profilePicture);
+    }).then((profilePictureUploadTask) => {
+      return profilePictureUploadTask.ref.getDownloadURL();
+    }).then((downloadURL) => {
+      return firebase.firestore().collection('user').doc(_user.uid).set({username, birthDate, profilePictureURL: downloadURL});
+    }).then(() => undefined);
+  }
+
+  /**
+   * Login in Cargonaut user
+   * @param email {string}
+   * @param password {string}
+   * @returns {Promise<void>}
+   */
+  loginUser(email, password) {
+    return firebase.auth().signInWithEmailAndPassword(email, password).then(() => undefined);
+  }
+
+  /**
+   * Gets an Cargonaut user by id
+   * @param id {string}
+   * @returns {Promise<object>}
+   */
+  getUserByID(id) {
+    return firebase.firestore().collection('user').doc(id).get()
+    .then((doc) => doc.data());
+  }
+
+  /**
+   * Gets Entries for a user
+   * @param userID {string}
+   * @returns {Promise<Array<{id: string, data: object}>>}
+   */
+  getEntriesForUser(userID) {
+    return this._getXForUser("entry", "creator", userID);
+  }
+  /**
+   * Gets Vehicles for a user
+   * @param userID {string}
+   * @returns {Promise<Array<{id: string, data: object}>>}
+   */
+  getVehiclesForUser(userID) {
+    return this._getXForUser("vehicle", "owner", userID);
+  }
+  /**
+   * Gets Reviews for a user
+   * @param userID {string}
+   * @returns {Promise<Array<{id: string, data: object}>>}
+   */
+  getReviewsForUser(userID) {
+    return this._getXForUser("review", "for", userID);
+  }
+  /**
+   * Gets Entries from a user
+   * @param userID {string}
+   * @returns {Promise<Array<{id: string, data: object}>>}
+   */
+  getReviewsFromUser(userID) {
+    return this._getXForUser("review", "from", userID);
+  }
+  /**
+   * Gets Offers from and to a user
+   * @param userID {string}
+   * @returns {Promise<Array<{id: string, data: object}>>}
+   */
+  getOffersForUser(userID) {
+    return Promise.all([this._getXForUser("offer", "createdBy", userID),
+      this._getXForUser("offer", "createdFor", userID)]).then((offers) => {
+        return offers.flat();
+      });
+  }
+
+  /**
+   * Create a new cargonaut entry
+   * @param type {string} - Possible values: drive, driveRequest
+   * @param fromCity {string}
+   * @param toCity {string}
+   * @param departureTime {Date}
+   * @param arrivalTime {Date}
+   * @param price {number}
+   * @param vehicleID {string}
+   * @param creatorID {string}
+   * @returns {Promise<void>}
+   */
+  createEntry(type, fromCity, toCity, departureTime, arrivalTime, price, vehicleID, creatorID) {
+    const vehicle = firebase.firestore().collection('vehicle').doc(vehicleID);
+    const creator = firebase.firestore().collection('creator').doc(creatorID);
+    return firebase.firestore().collection('entry').add({
+      type,
+      fromCity,
+      toCity,
+      departureTime,
+      arrivalTime,
+      price,
+      vehicle,
+      creator,
+    }).then(() => undefined);
+  }
+
+  /**
+   * Internal method for user queries on x
+   * @param x <string>
+   * @param userFieldName <string>
+   * @param userID <string>
+   * @returns {Promise<Array<{id: string, data: object}>>}
+   * @private
+   */
+  _getXForUser(x, userFieldName, userID) {
+    const userRef = firebase.firestore().collection('user').doc(userID);
+    return firebase.firestore().collection(x).where(userFieldName, "==", userRef).get()
+      .then((snapshot) => snapshot.docs.map((doc) => {
+        return {id: doc.id, data: doc.data()}
+      })
+    );
+  }
+}
